a10a96add595e8d018cbe863255b68d8c62a28a2
Charlie <bilzt20@gmail.com>
2022-07-10 10:48:40 +1000

    change color of bg on odd/even

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-export default class App extends Component {
+
+
+export default class App extends Component<any, any> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      count: 6
+    }
+  }
+
+  isEven = (val: number) => {
+    return val % 2 === 0 ? "Even" : "Odd";
+  }
+
+  getClassName = (val: number) => {
+    let bgColor = val % 2 === 0 ? "bg-primary" : "bg-secondary"
+    return bgColor + " text-white text center p-2 m-1"
+  }
+
   render = () =>
-    <h4 className='bg-primary text-white text-center p-2 m-1'>Component Element</h4>
+    <h4 className={this.getClassName(this.state.count)}>Number of things: {this.isEven(this.state.count)}</h4>
 }
